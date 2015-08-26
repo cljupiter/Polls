d72dcfc65e4615f38a32f3fcc5e3b7740c0ee0e1
cljupiter <cesar.lie.dev@gmail.com>
2015-08-26 16:23:39 +0300

    Using 'Question' without 'Poll' -- now server runs

--- polls/admin.py
+++ polls/admin.py
@@ -3,20 +3,19 @@ from django.contrib import admin
 # Register your models here.
 # these lines added:
 
-from polls.models import Choice
+from polls.models import Question, Choice
 
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
-class PollAdmin(admin.ModelAdmin):
+class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question']}),
         ('Date information',{'fields': ['pub_date'], 'classes': ['collapse']})
     ]
     inlines = [ChoiceInline]
 
-from polls.models import Polls
-admin.site.register(Poll, PollAdmin)
+admin.site.register(Question, QuestionAdmin)
 
