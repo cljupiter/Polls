d9614189259b097c64277544e36c869f7dd36911
cljupiter <cesar.lie.dev@gmail.com>
2015-08-31 14:26:19 +0300

    Fixed errors -- now server runs

--- polls/views.py
+++ polls/views.py
@@ -8,31 +8,23 @@ from django.utils import timezone
 from django.views import generic
 from django.template import RequestContext, loader
 from .models import Choice, Question
-"""
+
 class IndexView(generic.ListView):
-    model = Question
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-    def get_request(self):
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ) .order_by('-pub_date')[:5]
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
 
-    def get_queryset(self):
-
-        #Excludes any question that aren't published yet.
-
-        return Question.objects.filter(pub_date__lte=timezone.now())
-
 class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
-"""
+
 def vote(request, question_id):
     p=get_object_or_404(Question, pk=question_id)
     try:
@@ -51,16 +43,3 @@ def vote(request, question_id):
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(p.id, )))
     return HttpResponse("You're voting on question %s." % question_id)
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list,}
-    return render(request, 'polls/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
