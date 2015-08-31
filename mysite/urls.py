f7820b24c11f72680fcb48ec915727ddc397f1d0
cljupiter <cesar.lie.dev@gmail.com>
2015-08-31 12:29:21 +0300

    Added 'namespace'

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
 
-    url(r'^polls/', include('polls.urls')),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
 
     url(r'^admin/', include(admin.site.urls)),
 ]
